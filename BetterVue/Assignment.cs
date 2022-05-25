e4279a049a870988fe38eaa3c97744b7cc2c0c80
Gymhgy <49801033+Gymhgy@users.noreply.github.com>
2022-05-24 21:41:34 -0700

    Grade Calculator

---
+++ BetterVue/Assignment.cs
@@ -0,0 +1,15 @@
+﻿namespace BetterVue {
+    public class Assignment {
+        public Assignment(string type, string originalPointVal, string points, string total) {
+            Type = type;
+            OriginalPointVal = originalPointVal;
+            Points = points;
+            Total = total;
+        }
+
+        public string Type { get; }
+        public string OriginalPointVal { get; }
+        public string Points { get; set; }
+        public string Total { get; }
+    }
+}
